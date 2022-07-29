997eedea176ed6b56b5a4e5c69b5c5453d782af8
Alexandre Rehder <alexandrerehder@hotmail.com>
2022-07-29 12:23:32 -0300

    pom.xml

--- src/main/java/br/com/api/ApiBoot.java
+++ src/main/java/br/com/api/ApiBoot.java
@@ -1,2 +1,16 @@
-package br.com.api;public class ApiBoot {
+package br.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = { "br.com.api" })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class ApiBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBoot.class, args);
+    }
+
 }
